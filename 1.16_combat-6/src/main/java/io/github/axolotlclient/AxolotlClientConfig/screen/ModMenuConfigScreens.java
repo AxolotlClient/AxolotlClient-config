79c0aed4e38275cee0001e7f15bf21c50bf8e2b1
moehreag <moehreag@gmail.com>
2023-02-12 16:31:26 +0100

    fix modmenu compat on 1.16
    - package fapi modules with the mod (1.16)
    - update qsl (1.19.2)

--- 1.16_combat-6/src/main/java/io/github/axolotlclient/AxolotlClientConfig/screen/ModMenuConfigScreens.java
+++ 1.16_combat-6/src/main/java/io/github/axolotlclient/AxolotlClientConfig/screen/ModMenuConfigScreens.java
@@ -1,8 +1,8 @@
 package io.github.axolotlclient.AxolotlClientConfig.screen;
 
-import com.terraformersmc.modmenu.api.ConfigScreenFactory;
-import com.terraformersmc.modmenu.api.ModMenuApi;
 import io.github.axolotlclient.AxolotlClientConfig.AxolotlClientConfigManager;
+import io.github.prospector.modmenu.api.ConfigScreenFactory;
+import io.github.prospector.modmenu.api.ModMenuApi;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -11,7 +11,7 @@ public class ModMenuConfigScreens implements ModMenuApi {
 
     @Override
     public Map<String, ConfigScreenFactory<?>> getProvidedConfigScreenFactories() {
-        HashMap<String, ConfigScreenFactory<?>> factories = new HashMap<>();
+        Map<String, ConfigScreenFactory<?>> factories = new HashMap<>();
         AxolotlClientConfigManager.getInstance().getConfigs().forEach((s, configHolder) ->
                 factories.put(s, parent ->
                         AxolotlClientConfigManager.getInstance().getConfigScreen(s, parent)));
