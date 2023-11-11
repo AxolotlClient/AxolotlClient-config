5c2f04c15946b23ab4b0e6ab677a2258d9d7b20d
moehreag <moehreag@gmail.com>
2023-11-11 13:22:25 +0100

    I'd like an explanation why this works

--- 1.19.2/src/main/java/io/github/axolotlclient/AxolotlClientConfig/impl/ui/rounded/NVGUtil.java
+++ 1.19.2/src/main/java/io/github/axolotlclient/AxolotlClientConfig/impl/ui/rounded/NVGUtil.java
@@ -2,11 +2,15 @@ package io.github.axolotlclient.AxolotlClientConfig.impl.ui.rounded;
 
 import java.util.function.Consumer;
 
+import com.mojang.blaze3d.vertex.VertexBuffer;
 import io.github.axolotlclient.AxolotlClientConfig.impl.ui.NVGMC;
 
 public class NVGUtil {
 
 	public static void wrap(Consumer<Long> function) {
+		VertexBuffer buffer = new VertexBuffer();
+		buffer.bind();
+		buffer.close();
 		NVGMC.wrap(function);
 	}
 }
