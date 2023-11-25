bb21f856c26150cd4f41ae0a7b1de6e27dab5b5f
moehreag <moehreag@gmail.com>
2023-11-25 10:32:30 +0100

    add license headers

--- common/src/main/java/io/github/axolotlclient/AxolotlClientConfig/impl/ui/NVGFont.java
+++ common/src/main/java/io/github/axolotlclient/AxolotlClientConfig/impl/ui/NVGFont.java
@@ -1,19 +1,23 @@
 /*
- * Sol Client - an open source Minecraft client
- * Copyright (C) 2021-2023  TheKodeToad and Contributors
+ * Copyright © 2021-2023 moehreag <moehreag@gmail.com> & Contributors
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * This file is part of AxolotlClient.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * For more information, see the LICENSE file.
  */
 
 package io.github.axolotlclient.AxolotlClientConfig.impl.ui;
